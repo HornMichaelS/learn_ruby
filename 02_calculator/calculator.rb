b907d747dc2ce084faebf4a176c0c5a02c1fd9b8
Michael Horn <hornmichaels@gmail.com>
2019-08-25 15:46:11 -0700

    Finished 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+
+def add m, n
+    m + n
+end
+
+def subtract m, n
+    m - n
+end
+
+def sum arr
+    result = 0
+    arr.each { |num| result += num }
+    result
+end
+
+def multiply m, n, *rest
+    result = m * n
+    rest.each { |num| result *= num }
+    result
+end
+
+def power m, n
+    result = 1
+    n.times do
+        result *= m
+    end
+    result
+end
+
+def factorial n
+    if n < 2
+        return 1
+    end
+
+    n * factorial(n - 1)
+end
