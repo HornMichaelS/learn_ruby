e48c60d9663ced8da1a03aecd890bbb9050ec36d
Michael Horn <hornmichaels@gmail.com>
2019-08-27 05:28:54 -0700

    Finished 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,31 @@
 class Book
-# write your code here
+  def initialize
+    @small_words = [
+      'for', 'and', 'nor', 'but', 'or', 'yet', 'so',
+      'aboard', 'about', 'above', 'absent', 'across', 'after', 'against', 'along',
+      'alongside', 'amid', 'amidst', 'among', 'amongst', 'around', 'as', 'astride',
+      'at', 'atop', 'before', 'afore', 'behind', 'below', 'beneath', 'beside',
+      'besides', 'between', 'beyond', 'by', 'circa', 'despite', 'down', 'during',
+      'except', 'for', 'from', 'in', 'inside', 'into', 'less', 'like', 'minus',
+      'near', 'nearer', 'nearest', 'notwithstanding', 'of', 'off', 'on', 'onto',
+      'opposite', 'outside', 'over', 'past', 'per', 'save', 'since', 'through',
+      'throughout', 'to', 'toward', 'towards', 'under', 'underneath', 'until', 'up',
+      'upon', 'upside', 'versus', 'via', 'with', 'within', 'without', 'worth',
+      'the', 'a', 'an', 'some'
+    ]
+  end
+
+  def title
+    @title
+  end
+
+  def title=(title)
+    @title = title.split.map.with_index do |word, index| 
+      if index != 0 and @small_words.include? word
+        word
+      else
+        word.capitalize
+      end
+    end.join ' '
+  end
 end
